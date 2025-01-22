602141c5af0c2c741e6e67545bbda671d47c4714
umar ali <aliumar@pearlorganisation.com>
2025-01-22 16:39:07 +0530

    add security and CORS filtering

---
+++ src/main/java/com/documentor/amazon/Config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.documentor.amazon.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("http://localhost:5555");
+        config.addAllowedOriginPattern("file://*");
+        config.addAllowedOriginPattern("http://localhost:3000/");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/documentor/amazon/Config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.documentor.amazon.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableWebSecurity
+@EnableTransactionManagement
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .contentSecurityPolicy(csp -> csp
+                                .policyDirectives("default-src 'self';" +
+                                        "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com;" +
+                                        "style-src 'self' 'unsafe-inline';" +
+                                        "connect-src 'self' http://localhost:5555 wss://api.pilling.in file://*;")
+                        )
+                )
+                .authorizeHttpRequests(authorizationRequests -> authorizationRequests
+                        // Add /verifyUser to accessible endpoints with appropriate authority
+                        .requestMatchers("/api/v0/**","/api/v0").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
+
+
