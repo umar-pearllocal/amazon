bf101fd1fb6c780a049d07aaf5009af0f93134ea
umar ali <aliumar@pearlorganisation.com>
2025-01-22 16:38:01 +0530

    Custom Handler for historical Order DATA

---
+++ src/main/java/com/documentor/amazon/Entity/MapListJsonConverter.java
@@ -0,0 +1,35 @@
+package com.documentor.amazon.Entity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Converter
+public class MapListJsonConverter implements AttributeConverter<List<Map<String, Object>>, String> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<Map<String, Object>> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting map list to JSON", e);
+        }
+    }
+
+    @Override
+    public List<Map<String, Object>> convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, new TypeReference<List<Map<String, Object>>>() {});
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Error reading JSON to map list", e);
+        }
+    }
+}
